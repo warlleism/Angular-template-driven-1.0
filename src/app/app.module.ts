d724ee6cc5a9fbfe3790330559b0c76c6607604f
Warllei Martins <warllei.s.martins@hotmail.com>
2021-11-29 06:11:17 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { CampoControlErroComponent } from './campo-control-erro/campo-control-erro.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateFormComponent,
+    CampoControlErroComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/template-form/template-form.component.ts
@@ -0,0 +1,60 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-form',
+  templateUrl: './template-form.component.html',
+  styleUrls: ['./template-form.component.css'],
+})
+export class TemplateFormComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {}
+
+  onSubmit(form: any) {
+    console.log(form);
+  }
+
+  verificaValidTouched(campo: any) {
+    return campo.invalid && campo.touched;
+  }
+
+  aplicaCssErro(campo: any) {
+    return {
+      'has-error': this.verificaValidTouched(campo),
+      'has-feedback': this.verificaValidTouched(campo),
+    };
+  }
+
+  consultaCEP(cep: any, form: any) {
+    cep = cep.replace(/\D/g, '');
+
+    if (cep != '') {
+      var validacep = /^[0-9]{8}$/;
+
+      if (validacep.test(cep)) {
+        this.http
+          .get(`https://viacep.com.br/ws/${cep}/json`)
+          .subscribe((data) => {
+            this.populaDadosForm(data, form);
+          });
+      }
+    }
+  }
+
+  populaDadosForm(dados: any, form: any) {
+    form.setValue({
+      nome: null,
+      email: null,
+      endereco: {
+        rua: dados.logradouro,
+        cep: dados.cep,
+        numero: ' ',
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf,
+      },
+    });
+  }
+}
